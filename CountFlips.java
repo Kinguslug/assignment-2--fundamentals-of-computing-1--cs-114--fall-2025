e460c95ca21143175a4a20638e9fbaadfdd8d286
Kinguslug <hawesomeness1@gmail.com>
2025-10-06 16:02:10 -0400

    Finished the count flip

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,19 @@
 public class CountFlips {
   public static void main(String[] args) {
+    Coin myCoin = new Coin();
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    for (int i = 1; i <= 100; i++){
+      myCoin.flip();
+      if (myCoin.isHeads()) {
+        headsCount += 1;
+      }
+      else{
+        tailsCount += 1;
+      }
+    }
+    System.out.println("\nHeads count: "+headsCount+"\nTails count: "+tailsCount);
   }
+
 }
