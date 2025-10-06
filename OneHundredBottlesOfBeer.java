f9b62afd8b9721e9df7b1c7554d27ab8cc6fb2f0
Kinguslug <hawesomeness1@gmail.com>
2025-10-06 16:38:02 -0400

    Bottle Of Beer finished

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,11 @@
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    int bottleOfBeer = 100;
+    while (bottleOfBeer > -1) {
+      System.out.println(bottleOfBeer + " bottles of beer on the wall");
+      System.out.println(bottleOfBeer + " bottles of beer");
+      System.out.println("If one of those bottles should happen to fall\n");
+      --bottleOfBeer;
+    }
   }
 }
