f8a570ae7a32e792006829e61b26c7ce8d90ffb7
Kinguslug <hawesomeness1@gmail.com>
2025-10-06 17:14:59 -0400

    Small syntax fix

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,11 +1,13 @@
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
     int bottleOfBeer = 100;
-    while (bottleOfBeer > -1) {
+
+    while (bottleOfBeer > 0) {
       System.out.println(bottleOfBeer + " bottles of beer on the wall");
       System.out.println(bottleOfBeer + " bottles of beer");
-      System.out.println("If one of those bottles should happen to fall\n");
+      System.out.println("If one of those bottles should happen to fall");
       --bottleOfBeer;
+      System.out.println(bottleOfBeer + " bottles of beer on the wall\n");
     }
   }
 }
